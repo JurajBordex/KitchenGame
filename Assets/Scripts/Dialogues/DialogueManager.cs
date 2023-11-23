c3712899d4ae43086608243a7fc7fea3c6b4a5a2
TheTourchGuy <amahmadmuaz@gmail.com>
2023-11-23 12:04:20 +0300

    Progress

--- Assets/Scripts/Dialogues/DialogueManager.cs
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     Queue<string> dialogueSentence;
 
+    [SerializeField] TextMeshPro npcNameText;
+    [SerializeField] TextMeshPro dialogueText;
+
     void Start()
     {
         dialogueSentence = new Queue<string>();
+        
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log(dialogue.npcName);
+        Debug.Log("Starting Conversation with " + dialogue.npcName);
+
+        npcNameText.text = dialogue.npcName;
+
+        dialogueSentence.Clear();
+
+        foreach(string sentence in dialogue.dialogueBoxes)
+        {
+            dialogueSentence.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(dialogueSentence.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = dialogueSentence.Dequeue();
+        Debug.Log(sentence);
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of Conversation");
     }
 }
