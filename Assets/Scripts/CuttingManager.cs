0ffab24f18a858bad49771468c88eed8a0fa242d
JurajBordex <bordex.dev@gmail.com>
2023-11-26 21:59:37 +0100

    Managed SFX and started working on playing sfx from other scripts

--- Assets/Scripts/CuttingManager.cs
+++ Assets/Scripts/CuttingManager.cs
@@ -13,15 +13,21 @@ public class CuttingManager : MonoBehaviour
 	public MoveableObject moveableScript;
 	public Ingredient ingredientScript;
 
-	[SerializeField] private GameObject buttonCuttingToSlices, buttonCuttingToSmallestPieces; 
-	//GameObjects
-	//Vectors
+	private SFX sfx;
 
-	public void CutToSlices()
+	[SerializeField] private GameObject buttonCuttingToSlices, buttonCuttingToSmallestPieces;
+    //GameObjects
+    //Vectors
+    private void Start()
+    {
+		sfx = GameObject.FindGameObjectWithTag("SFX").GetComponent<SFX>();
+    }
+    public void CutToSlices()
     {
 		moveableScript.currentIngredientWeight = ingredientScript.cutToSlicesWeight; //change the weight
 		moveableScript.ingredientScript.state = 1; //change the state to 1
 		//change the visuals
+		//play sfx
 		buttonCuttingToSlices.SetActive(false); //setting just this button to not active in case the player wants to cut the vegetables again
 		buttonCuttingToSmallestPieces.transform.position = new Vector3(-15.44f, -2.44f, 0); //change position to more centered
     }
@@ -29,9 +35,11 @@ public class CuttingManager : MonoBehaviour
     {
 		moveableScript.currentIngredientWeight = ingredientScript.cutToSmallPiecesWeight; //change the weight
 		moveableScript.ingredientScript.state = 2; //change the state to 2
-		//change the visuals
-		//IF YOU WANT TO CHANGE THE LOCATION THAN ADD THE POSITION OF GameCanvas TO IT OR ELSE IT WON'T BE RIGHT PRECISE
-		buttonCuttingToSmallestPieces.transform.position = new Vector3(-12.83f, -2.44f, 0); //change position to right defualt side
+        //change the visuals
+        //play sfx
+
+        //IF YOU WANT TO CHANGE THE LOCATION THAN ADD THE POSITION OF GameCanvas TO IT OR ELSE IT WON'T BE RIGHT PRECISE
+        buttonCuttingToSmallestPieces.transform.position = new Vector3(-12.83f, -2.44f, 0); //change position to right defualt side
 		DisableButtons();
 	}
 	public void EnableButtonForSmallestPieces()
