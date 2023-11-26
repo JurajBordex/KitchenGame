eba5253eec949b3a837d5519d8b9e5d5c837c401
JurajBordex <bordex.dev@gmail.com>
2023-11-26 12:25:11 +0100

    Removed debugging code

--- Assets/Scripts/InteractableObjects/MoveableObject.cs
+++ Assets/Scripts/InteractableObjects/MoveableObject.cs
@@ -130,7 +130,6 @@ public class MoveableObject : MonoBehaviour
         if (canDrag && gameManager.isDragging)
         {
             transform.position = GetMouseWorldPosition() + mousePositionOffset;
-            Debug.Log("TRANS POS Z VALUE : " + transform.position.z);
         }
     }
     private void OnMouseUp()
