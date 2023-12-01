7156ce57a25b23bef73dcb7cbebbc1fd6af85c3a
JurajBordex <bordex.dev@gmail.com>
2023-12-01 22:31:17 +0100

    Some more changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
